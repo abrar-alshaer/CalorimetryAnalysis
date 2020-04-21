b5f45d14f78590b9d5305096d1bfaf7b21fb335f
Abrar Al-Shaer <abrar@email.unc.edu>
2020-04-21 13:06:16 -0400

    Update Calorimetry Analysis.R

--- Calorimetry Analysis.R
+++ Calorimetry Analysis.R
@@ -13,9 +13,9 @@ fileNames <- Sys.glob("*.csv")
 
 #create empty dataframe with column names for all 18 parameters calculated & 3 mouse identifiers (mouse ID, genotype, diet) - 21 total data points for each mouse
 #as we iterate through each file this dataframe will get populated with each row corresponding to a mouse
-all_params <- setNames(data.frame(matrix(ncol = 21, nrow = 0)), c("Mouse ID", "Genotype", "Diet", "RMR", "Resting Gox", "Resting Lox", "Total VO2", "Total VCO2", "Total EE", "Total Gox", 
-                                                                  "Total Lox", "VO2 Night", "VO2 Day", "VCO2 Night", "VCO2 Day", "EE Night", "EE Day",
-                                                                  "Gox Night", "Gox Day", "Lox Night", "Lox Day"))
+all_params <- setNames(data.frame(matrix(ncol = 24, nrow = 0)), c("Mouse ID", "Genotype", "Diet", "RMR", "Resting Gox", "Resting Lox", "Total VO2", "Total VCO2", "Total EE", "Total Gox", 
+                                                                  "Total Lox", "Total XT", "VO2 Night", "VO2 Day", "VCO2 Night", "VCO2 Day", "EE Night", "EE Day",
+                                                                  "Gox Night", "Gox Day", "Lox Night", "Lox Day", "XT Night", "XT Day"))
 
 #iterate through each file
 i = 1 #initializing counter
@@ -28,8 +28,13 @@ for (fileName in fileNames) {
   Genotype <- substr(fileName, start = 23, stop = 24)
   Diet <- substr(fileName, start = 26, stop = 28)
   
-  #Extracting all instances where the Xt is 0 (mouse is not moving)
-  Xt_zero <- calData[calData$XT == 0,]
+  #Extracting all instances where the Xt is 0 (mouse is not moving) or minimum Xt if Xt is never 0.
+  if(0 %in% calData$XT){
+    Xt_zero <- calData[calData$XT == 0,]
+  } else {
+    min_Xt <- min(calData$XT)
+    Xt_zero <- calData[calData$XT == min_Xt,]  
+    }
   
   #calculating mean resting metabolic rate (average of normalized energy expenditure at all instances Xt = 0)
   RMR <- mean(Xt_zero$EE.cal.min.kg.lbm)
@@ -44,12 +49,11 @@ for (fileName in fileNames) {
   total_EE <- mean(calData$EE.cal.min.kg.lbm) #total average for all EE measures
   total_Gox <- mean(calData$Gox.normalized) #total average for all Gox measures
   total_Lox <- mean(calData$Lox.normalized) #total average for all Lox measures
+  total_Xt <- mean(calData$XT) #total average for all Xt measures
   
-  #hours 19:06-6:57 is the night cycle, 7:06-18:57 is the day cycle
   #subsetting data based on 2 night cycles and 2 day cycles
-  #every 80 data points there is a transition from a night cycle to a day cycle & vice versa
-  night <- calData[c(1:80,161:240),] #19:06-6:57 (7:06pm - 6:57am) night 1 & night 2
-  day <- calData[c(81:160, 241:320),] #19:06-6:57 (7:06pm - 6:57am) day 1 & day 2
+  night <- calData[(calData$Cycle == "night1") | (calData$Cycle == "night2"),] #approx. 7pm-7am night 1 and night 2
+  day <- calData[(calData$Cycle == "day1") | (calData$Cycle == "day2"),]  #approx. 7am-7pm day 1 & day 2
   
   #calculating VO2, VCO2, energy expenditure (EE), & Glucose/lipid metabolism based on day/night cycles
   VO2_night <- mean(night$VO2.norm)
@@ -57,18 +61,20 @@ for (fileName in fileNames) {
   EE_night <- mean(night$EE.cal.min.kg.lbm)
   Gox_night <- mean(night$Gox.normalized)
   Lox_night <- mean(night$Lox.normalized)
+  Xt_night <- mean(night$XT)
   
   VO2_day <- mean(day$VO2.norm)
   VCO2_day <- mean(day$VCO2.norm)
   EE_day <- mean(day$EE.cal.min.kg.lbm)
   Gox_day <- mean(day$Gox.normalized)
   Lox_day <- mean(day$Lox.normalized)
+  Xt_day <- mean(day$XT)
   
   #add all the mouse info & calculated parameters to a dataframe
-  all_params[i,] <- c(MouseID, Genotype, Diet, RMR, Gox_rest, Lox_rest, total_VO2, total_VCO2, total_EE, total_Gox, total_Lox, VO2_night, VO2_day, VCO2_night, VCO2_day, EE_night, EE_day, Gox_night, Gox_day, Lox_night, Lox_day)
+  all_params[i,] <- c(MouseID, Genotype, Diet, RMR, Gox_rest, Lox_rest, total_VO2, total_VCO2, total_EE, total_Gox, total_Lox, total_Xt, VO2_night, VO2_day, VCO2_night, VCO2_day, EE_night, EE_day, Gox_night, Gox_day, Lox_night, Lox_day, Xt_night, Xt_day)
   i = i+1 #iterate to the next row in the dataframe
 }
 
 #write the final dataframe (all_params) to a csv file
-write.csv(all_params, "Calorimtery_ALX-FPR2_Calculated_Analysis.csv")
+write.csv(all_params, "Calorimtery_ALX-FPR2_Calculated2_Analysis_ALL_Mice_12Weeks-Old.csv")
 
